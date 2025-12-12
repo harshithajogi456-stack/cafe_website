2bd46b3bcdfad0449921e63d177c034f569706c6
harshithajogi456-stack <harshithajogi456@gmail.com>
2025-12-13 00:02:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function welcomeMessage() {
+    alert("Welcome to Café Aroma! ☕ Enjoy your visit!");
+}
+
+function showOffer() {
+    alert("🔥 Today's Offer: 15% Off on All Hot Beverages!");
+}
+
+function contactAlert() {
+    alert("Call us at +91 98765 43210 or email cafearoma@example.com 😊");
+}

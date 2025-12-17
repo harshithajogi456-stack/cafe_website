ae77289a2ca41451683e1d1695c76afb09d7f08f
harshithajogi456-stack <harshithajogi456@gmail.com>
2025-12-17 23:51:28 +0530

    Add files via upload

---
+++ cafe_website/script.js
@@ -0,0 +1,7 @@
+function showWelcome() {
+  alert("Welcome to Manipal Mug ☕ – We are delighted to have you here.");
+}
+
+function submitContactForm() {
+  alert("Thank you for contacting Manipal Mug. We will get back to you shortly.");
+}
